d34cb1191eb324a3a7e0df450a044ebbfa51011e
tthhiinnaa <bjbnldh3461@gmail.com>
2024-11-06 20:42:39 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.31 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,49 @@
+# def solution(keymap, targets):
+#     answer = []
+#     dicts = {}
+#     for keys in keymap:
+#         for i,j in enumerate(keys):
+#             if j not in dicts.keys():
+#                 dicts[j] = i
+#             else:
+#                 if dicts[j] > i:
+#                     dicts[j] = i
+#     for i in dicts:
+#         dicts[i] += 1
+#     for target in targets:
+#         answer_o = 0
+#         for s in target:
+#             if s not in dicts.keys():
+#                 return [-1]
+#             answer_o += dicts[s]
+#         answer.append(answer_o)
+
+#     return answer
+
+def solution(keymap, targets):
+    # 각 문자가 최소 몇 번 눌러야 하는지 저장할 딕셔너리
+    key_press_count = {}
+    
+    # keymap에서 각 문자에 대해 최소 키 입력 횟수를 계산
+    for keys in keymap:
+        for i, char in enumerate(keys):
+            if char not in key_press_count:
+                key_press_count[char] = i + 1
+            else:
+                key_press_count[char] = min(key_press_count[char], i + 1)
+    
+    # 각 target에 대해 최소 키 입력 횟수를 계산
+    result = []
+    for target in targets:
+        total_presses = 0
+        for char in target:
+            # 문자가 keymap에 없으면 작성할 수 없으므로 -1 리턴
+            if char not in key_press_count:
+                total_presses = -1
+                break
+            total_presses += key_press_count[char]
+        
+        result.append(total_presses)
+    
+    return result
+
