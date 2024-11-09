ce66e0e09fabc33da1c549e2daa4d13a7e667ed1
tthhiinnaa <bjbnldh3461@gmail.com>
2024-11-10 08:48:30 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.36 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -20,9 +20,35 @@
 
 #     return answer
 
-def solution(keymap, targets):
-    # 각 문자가 최소 몇 번 눌러야 하는지 저장할 딕셔너리
-    key_press_count = {}
+# def solution(keymap, targets):
+#     # 각 문자가 최소 몇 번 눌러야 하는지 저장할 딕셔너리
+#     key_press_count = {}
+    
+#     # keymap에서 각 문자에 대해 최소 키 입력 횟수를 계산
+#     for keys in keymap:
+#         for i, char in enumerate(keys):
+#             if char not in key_press_count:
+#                 key_press_count[char] = i + 1
+#             else:
+#                 key_press_count[char] = min(key_press_count[char], i + 1)
+    
+#     # 각 target에 대해 최소 키 입력 횟수를 계산
+#     result = []
+#     for target in targets:
+#         total_presses = 0
+#         for char in target:
+#             # 문자가 keymap에 없으면 작성할 수 없으므로 -1 리턴
+#             if char not in key_press_count:
+#                 total_presses = -1
+#                 break
+#             total_presses += key_press_count[char]
+        
+#         result.append(total_presses)
+    
+#     return result
+
+def solution(keymap,targets):
+    key_press_count = {} # 최소 알파벳의 입력 숫자
     
     # keymap에서 각 문자에 대해 최소 키 입력 횟수를 계산
     for keys in keymap:
@@ -31,19 +57,18 @@ def solution(keymap, targets):
                 key_press_count[char] = i + 1
             else:
                 key_press_count[char] = min(key_press_count[char], i + 1)
-    
-    # 각 target에 대해 최소 키 입력 횟수를 계산
     result = []
+    print(key_press_count)
     for target in targets:
-        total_presses = 0
-        for char in target:
-            # 문자가 keymap에 없으면 작성할 수 없으므로 -1 리턴
-            if char not in key_press_count:
-                total_presses = -1
+        result_sum = 0
+        for a in target:
+            if not a in key_press_count:
+                result_sum = -1
                 break
-            total_presses += key_press_count[char]
-        
-        result.append(total_presses)
-    
+            else:
+                result_sum += key_press_count[a]
+        result.append(result_sum)
     return result
-
+    
+    
+    
